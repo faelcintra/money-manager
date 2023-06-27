f1c07cda32be6f098d1ed4ffc23c3a1c59f7bcc4
faelcintra <rafaacintramartins@gmail.com>
2023-06-27 18:35:05 -0300

    feat: :sparkles: Implementation of custom summary hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,12 @@
-import { SummaryCard, SummaryContainer } from "./styles";
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 
+import { SummaryCard, SummaryContainer } from "./styles";
+import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
+
 export function Summary() {
+  const summary = useSummary();
+  
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -9,21 +14,21 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color={"#00B37E"} />
         </header>
-        <strong children={"R$ 16.500,00"} />
+        <strong children={priceFormatter.format(summary.income)} />
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Saidas</span>
           <ArrowCircleDown size={32} color={"#F75A68"} />
         </header>
-        <strong children={"R$ 16.500,00"} />
+        <strong children={priceFormatter.format(summary.outcome)} />
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
           <span>Entradas</span>
           <CurrencyDollar size={32} color={"#FAFAFA"} />
         </header>
-        <strong children={"R$ 16.500,00"} />
+        <strong children={priceFormatter.format(summary.total)} />
       </SummaryCard>
     </SummaryContainer>
   );
